f4f8bfc77f927db8909b1dc4036d6c80e6ea59b0
ST <workinvs@gmail.com>
2023-04-27 15:28:37 +0300

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -120,7 +120,7 @@ export default function Home() {
                           }
                           setLoading(false);
                         }}
-                      >{btnText}</button>
+                      >{loading ? 'Loading...' : btnText}</button>
                     ))
                   }
                 </div>
