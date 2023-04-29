e3e2cce417821e0bbe3ce50b0b34bff8cdc2e3a0
ST <workinvs@gmail.com>
2023-04-29 20:43:35 +0300

    add button logout

--- src/components/layout/common/Header.tsx
+++ src/components/layout/common/Header.tsx
@@ -1,22 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth';
 import {
   auth,
+  logout,
 } from '@/firebase';
 import { useRouter } from 'next/router'
 
 import { Container, Box, Button } from '@chakra-ui/react';
 import { FiLogIn } from "react-icons/fi";
 import { AiOutlineRollback } from "react-icons/ai";
+import { BiLogOut } from "react-icons/bi";
 
 function Header() {
-  const [user] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
   const { push, asPath } = useRouter();
+  useEffect(() => {
+    if (!user) return;
+  }, [user, loading, push, asPath]);
   return (
     <Box>
       <Container maxW='1536px' padding={2}>
         <Box display='flex' justifyContent='flex-end'>
-          {!user && asPath == '/' &&
+          {!user && !loading && !(asPath === '/login' || asPath === '/signup') &&
             <Button colorScheme='messenger' size='md' rightIcon={<FiLogIn size={20} />} onClick={() => push('/login')}>
               Login
             </Button>
@@ -24,6 +29,13 @@ function Header() {
           {(asPath === '/login' || asPath === '/signup') &&
             <Button colorScheme='messenger' size='md' leftIcon={<AiOutlineRollback size={20} />} onClick={() => push('/')}>
               Back to Home
+            </Button>
+          }
+          {user &&
+            <Button colorScheme='messenger' size='md' rightIcon={<BiLogOut size={20} />} onClick={() => {
+              logout();
+            }} >
+              Logout
             </Button>
           }
         </Box>
