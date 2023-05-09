fc330228177d74a50d1d8c8643885e0322101c74
ST <workinvs@gmail.com>
2023-05-09 18:47:55 +0300

    fix

--- src/pages/api/checkoutStripe.ts
+++ src/pages/api/checkoutStripe.ts
@@ -3,14 +3,11 @@ import { updateDoc, doc } from 'firebase/firestore';
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === "POST") {
-        const { type, id, data } = req.body as any;
-        const userId = data.client_reference_id as string;
-        const docRef = doc(db, "users", userId);
-        updateDoc(docRef, {
-            isPremium: true
-        });
-        res.status(200).json({ type, id });
-    }
-
+    const { type, id, data } = req.body as any;
+    const userId = data.client_reference_id as string;
+    const docRef = doc(db, "users", userId);
+    updateDoc(docRef, {
+        isPremium: true
+    });
+    res.status(200).json({ type, id });
 }
