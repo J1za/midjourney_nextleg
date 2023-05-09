426b84c27344c3a510544def4e15d756e7903cae
ST <workinvs@gmail.com>
2023-05-09 18:18:02 +0300

    fix webhook

--- src/pages/api/checkoutStripe.ts
+++ src/pages/api/checkoutStripe.ts
@@ -9,31 +9,26 @@ const stripe = new Stripe(`${process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY}`, {
 });
 const webhookSecret: any = process.env.NEXT_PUBLIC_SIGNING_SECRET_KEY;
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === "POST") {
-        const buf = await buffer(req);
-        const sig: any = req.headers["stripe-signature"];
+    const buf = await buffer(req);
+    const sig: any = req.headers["stripe-signature"];
 
-        let event;
+    let event;
 
-        try {
-            event = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
-        } catch (err: any) {
-            res.status(400).send(`Webhook Error: ${err.message}`);
-            return;
-        }
-        if (event.type === "charge.succeeded") {
-            const charge = event.data.object;
-            const docRef = doc(db, "users", 'EfWSGJmgS9NU5M2ki5IEJsEYwAe2');
-            updateDoc(docRef, {
-                isPremium: true
-            });
-            alert('yes true done good well done nice')
-        } else {
-            console.warn(`Unhandled event type: ${event.type}`);
-        }
-        res.json({ received: true });
-    } else {
-        res.setHeader("Allow", "POST");
-        res.status(405).end("Method Not Allowed");
+    try {
+        event = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
+    } catch (err: any) {
+        res.status(400).send(`Webhook Error: ${err.message}`);
+        return;
     }
+    if (event.type === "charge.succeeded") {
+        const charge = event.data.object;
+        const docRef = doc(db, "users", 'EfWSGJmgS9NU5M2ki5IEJsEYwAe2');
+        updateDoc(docRef, {
+            isPremium: true
+        });
+        alert('yes true done good well done nice')
+    } else {
+        console.warn(`Unhandled event type: ${event.type}`);
+    }
+    res.json({ received: true });
 }
