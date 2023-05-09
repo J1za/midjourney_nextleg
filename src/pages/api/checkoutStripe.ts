da5818adaac4598f52e1d1162dc6a4eda6ef7eb5
ST <workinvs@gmail.com>
2023-05-09 18:59:46 +0300

    update

--- src/pages/api/checkoutStripe.ts
+++ src/pages/api/checkoutStripe.ts
@@ -1,15 +1,14 @@
 import { db } from '@/firebase';
-import { updateDoc, doc } from 'firebase/firestore';
+import { updateDoc, setDoc, doc } from 'firebase/firestore';
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const { type, id, data } = req.body as any;
     const userId = data?.client_reference_id as string;
     if (userId) {
-        const docRef = doc(db, "users", userId);
-        updateDoc(docRef, {
+        setDoc(doc(db, "users", userId), {
             isPremium: true
-        });
+        }, { merge: true });
     }
     res.status(200).json({ type, id });
 }
