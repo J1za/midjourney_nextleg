0b85f131f9b3c78246bf83b45bfa25a22c9d4d40
ST <workinvs@gmail.com>
2023-05-09 18:51:16 +0300

    fix

--- src/pages/api/checkoutStripe.ts
+++ src/pages/api/checkoutStripe.ts
@@ -4,10 +4,12 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const { type, id, data } = req.body as any;
-    const userId = data.client_reference_id as string;
-    const docRef = doc(db, "users", userId);
-    updateDoc(docRef, {
-        isPremium: true
-    });
+    const userId = data?.client_reference_id as string;
+    if (userId) {
+        const docRef = doc(db, "users", userId);
+        updateDoc(docRef, {
+            isPremium: true
+        });
+    }
     res.status(200).json({ type, id });
 }
