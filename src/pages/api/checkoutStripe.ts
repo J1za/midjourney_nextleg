c7133d768cba732bf04e79416dda6401e5ca6d39
ST <workinvs@gmail.com>
2023-05-10 13:49:31 +0300

    check hook

--- src/pages/api/checkoutStripe.ts
+++ src/pages/api/checkoutStripe.ts
@@ -1,21 +1,37 @@
+import { buffer } from "micro";
+import Stripe from 'stripe';
 import { db } from '@/firebase';
 import { setDoc, doc } from 'firebase/firestore';
 import { NextApiRequest, NextApiResponse } from "next";
 
+const stripe = new Stripe(`${process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY}`, {
+    apiVersion: '2022-11-15'
+});
+const webhookSecret: any = process.env.NEXT_PUBLIC_SIGNING_SECRET_KEY;
 export default async function handlerCheckout(req: NextApiRequest, res: NextApiResponse) {
-    const { type, id, data } = req.body as any;
-    if (type === 'checkout.session.completed') {
-        const userId = data!.client_reference_id as string;
+    if (req.method === "POST") {
+        const buf = await buffer(req);
+        const sig: any = req.headers["stripe-signature"];
+        let stripeEvent: any;
         try {
+            stripeEvent = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
+            console.log('stripeEvent', stripeEvent);
+        } catch (err: any) {
+            res.status(400).send(`Webhook Error: ${err.message}`);
+            return;
+        }
+
+        if ('checkout.session.completed' === stripeEvent.type) {
+            const session = stripeEvent.data;
+            const userId = stripeEvent.data.client_reference_id;
             await setDoc(doc(db, "users", userId), {
                 isPremium: true
             }, { merge: true });
-            res.status(200).json({ status: 'done' });
-        } catch (error) {
-            // Handle or log the error
-            res.status(400).json({ "Error updating Firestore document:": error });
+            res.json({ received: session });
+        } else {
+            res.setHeader("Allow", "POST");
+            res.status(405).end("Method Not Allowed");
         }
-    }
-    res.status(200).json({ status: 'done' });
 
+    }
 }
