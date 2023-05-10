9cb6fc099bf73cd24aa0550814b98cbfb5adc379
ST <workinvs@gmail.com>
2023-05-10 13:08:34 +0300

    fix

--- src/pages/api/checkoutStripe.ts
+++ src/pages/api/checkoutStripe.ts
@@ -6,10 +6,10 @@ export default async function handlerCheckout(req: NextApiRequest, res: NextApiR
     const { type, id, data } = req.body as any;
     const userId = data!.client_reference_id as string;
     try {
-        await setDoc(doc(db, "users", userId), {
+        await setDoc(doc(db, "users", 'EjzwZsV9ypchjrl6iVM4TeErjBH2'), {
             isPremium: true
         }, { merge: true });
-        res.status(200).json({ type, data: data });
+        res.status(200).json({ type, data });
     } catch (error) {
         // Handle or log the error
         res.status(400).json({ "Error updating Firestore document:": error });
