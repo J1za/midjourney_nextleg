5de9c986a5029491b1459855d4313ca62f8a2add
ST <workinvs@gmail.com>
2023-05-10 13:15:59 +0300

    fix errors

--- src/pages/api/checkoutStripe.ts
+++ src/pages/api/checkoutStripe.ts
@@ -4,15 +4,18 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handlerCheckout(req: NextApiRequest, res: NextApiResponse) {
     const { type, id, data } = req.body as any;
-    const userId = data!.client_reference_id as string;
-    try {
-        await setDoc(doc(db, "users", 'EjzwZsV9ypchjrl6iVM4TeErjBH2'), {
-            isPremium: true
-        }, { merge: true });
-        res.status(200).json({ type, data });
-    } catch (error) {
-        // Handle or log the error
-        res.status(400).json({ "Error updating Firestore document:": error });
+    if (type === 'checkout.session.completed') {
+        const userId = data!.client_reference_id as string;
+        try {
+            await setDoc(doc(db, "users", userId), {
+                isPremium: true
+            }, { merge: true });
+            res.status(200).json({ status: 'done' });
+        } catch (error) {
+            // Handle or log the error
+            res.status(400).json({ "Error updating Firestore document:": error });
+        }
     }
+
 
 }
